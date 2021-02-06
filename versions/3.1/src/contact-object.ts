4e09dba38d24782d8b5278d25a684f9ac4109a60
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-02-06 18:11:36 +0200

    Added ContactObject

--- versions/3.1/src/contact-object.ts
+++ versions/3.1/src/contact-object.ts
@@ -1,1 +1,40 @@
-export class ContactObject {}
+/**
+ * Contact information for the exposed API.
+ * 
+ * This object MAY be extended with [Specification Extensions][1].
+ * 
+ * [1]: https://swagger.io/specification/#specificationExtensions
+ * 
+ * ### Example with JSON
+ * 
+```json
+{
+  "name": "API Support",
+  "url": "https://www.example.com/support",
+  "email": "support@example.com"
+}
+```
+ * 
+ * ### Example with YAML
+ * 
+```yaml
+name: API Support
+url: https://www.example.com/support
+email: support@example.com
+```
+ */
+export class ContactObject {
+  /**
+   * The identifying name of the contact person/organization.
+   */
+  name?: string;
+  /**
+   * The URL pointing to the contact information. MUST be in the format of a URL.
+   */
+  url?: string;
+  /**
+   * The email address of the contact person/organization.
+   * MUST be in the format of an email address.
+   */
+  email?: string;
+}

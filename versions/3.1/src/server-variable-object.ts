1a0c4b80d200ba91ea7e4881357f0202b76978b8
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-02-06 18:51:12 +0200

    Added ServerVariableObject

--- versions/3.1/src/server-variable-object.ts
+++ versions/3.1/src/server-variable-object.ts
@@ -1,1 +1,31 @@
-export class ServerVariableObject {}
+/**
+ * An object representing a Server Variable for server URL template substitution.
+ * 
+ * This object MAY be extended with [Specification Extensions][1].
+ * 
+ * [1]: https://swagger.io/specification/#specificationExtensions
+ */
+export class ServerVariableObject {
+  /**
+   * An enumeration of string values to be used if the substitution options are from a limited set.
+   * The array MUST NOT be empty.
+   */
+  enum?: string[];
+  /**
+   * The default value to use for substitution, which SHALL be sent if an alternate value
+   * is _not_ supplied. Note this behavior is different than the [Schema Object's][1]
+   * treatment of default values, because in those cases parameter values are optional.
+   * If the [`enum`][2] is defined, the value MUST exist in the enum's values.
+   * 
+   * [1]: https://swagger.io/specification/#schemaObject
+   * [2]: https://swagger.io/specification/#serverVariableEnum
+   */
+  default: string;
+  /**
+   * An optional description for the server variable.
+   * [CommonMark syntax][1] MAY be used for rich text representation.
+   * 
+   * [1]: https://spec.commonmark.org/
+   */
+  description?: string;
+}

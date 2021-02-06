5fe2c9316d7c82102163e1c50c4ee3d3c3baa156
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-02-07 00:10:59 +0200

    Added ExternalDocumentationObject

--- versions/3.1/src/external-documentation-object.ts
+++ versions/3.1/src/external-documentation-object.ts
@@ -1,1 +1,36 @@
-export class ExternalDocumentationObject {}
+/**
+ * Allows referencing an external resource for extended documentation.
+ * 
+ * ### External Documentation Object Example
+ * 
+ * - Example with JSON
+```json
+{
+  "description": "Find more info here",
+  "url": "https://example.com"
+}
+```
+ * 
+ * - Example with YAML
+```yaml
+description: Find more info here
+url: https://example.com
+```
+ * 
+ * This object MAY be extended with [Specification Extensions][1].
+ * 
+ * [1]: https://swagger.io/specification/#specificationExtensions
+ */
+export class ExternalDocumentationObject {
+  /**
+   * A short description of the target documentation.
+   * [CommonMark syntax][1] MAY be used for rich text representation.
+   * 
+   * [1]: https://spec.commonmark.org/
+   */
+  description?: string;
+  /**
+   * The URL for the target documentation. Value MUST be in the format of a URL.
+   */
+  url: string;
+}

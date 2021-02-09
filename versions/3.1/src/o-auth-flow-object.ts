3e5adb9a64ecb55be6b24954d888af67bc0a186c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-02-09 10:07:20 +0200

    Added OAuthFlowsObject and OAuthFlowObject

---
+++ versions/3.1/src/o-auth-flow-object.ts
@@ -0,0 +1,80 @@
+/**
+ * Configuration details for a supported OAuth Flow.
+ * 
+ * ### OAuth Flow Object Examples
+ * 
+ * 
+```JSON
+{
+  "type": "oauth2",
+  "flows": {
+    "implicit": {
+      "authorizationUrl": "https://example.com/api/oauth/dialog",
+      "scopes": {
+        "write:pets": "modify pets in your account",
+        "read:pets": "read your pets"
+      }
+    },
+    "authorizationCode": {
+      "authorizationUrl": "https://example.com/api/oauth/dialog",
+      "tokenUrl": "https://example.com/api/oauth/token",
+      "scopes": {
+        "write:pets": "modify pets in your account",
+        "read:pets": "read your pets"
+      }
+    }
+  }
+}
+```
+ * 
+ * 
+```yaml
+type: oauth2
+flows: 
+  implicit:
+    authorizationUrl: https://example.com/api/oauth/dialog
+    scopes:
+      write:pets: modify pets in your account
+      read:pets: read your pets
+  authorizationCode:
+    authorizationUrl: https://example.com/api/oauth/dialog
+    tokenUrl: https://example.com/api/oauth/token
+    scopes:
+      write:pets: modify pets in your account
+      read:pets: read your pets 
+```
+ *
+ * This object MAY be extended with [Specification Extensions][1].
+ * 
+ * [1]: https://swagger.io/specification/#specificationExtensions
+ */
+export class OAuthFlowObject {
+  /**
+   * Applies To `oauth2` (`"implicit"`, `"authorizationCode"`).
+   *
+   * The authorization URL to be used for this flow. This MUST be in the form of a URL.
+   * The OAuth2 standard requires the use of TLS.
+   */
+  authorizationUrl: string;
+  /**
+   * Applies To `oauth2` (`"password"`, `"clientCredentials"`, `"authorizationCode"`).
+   *
+   * The token URL to be used for this flow. This MUST be in the form of a URL.
+   * The OAuth2 standard requires the use of TLS.
+   */
+  tokenUrl: string;
+  /**
+   * Applies To `oauth2`.
+   * 
+   * The URL to be used for obtaining refresh tokens. This MUST be in the form of a URL.
+   * The OAuth2 standard requires the use of TLS.
+   */
+  refreshUrl?: string;
+  /**
+   * Applies To `oauth2`.
+   *
+   * The available scopes for the OAuth2 security scheme. A map between the scope name
+   * and a short description for it. The map MAY be empty.
+   */
+  scopes: { [scopeName: string]: string };
+}
--- versions/3.1/src/o-auth-flows-object.ts
+++ versions/3.1/src/o-auth-flows-object.ts
@@ -1,1 +1,29 @@
-export class OAuthFlowsObject {}
+import { OAuthFlowObject } from './o-auth-flow-object';
+
+/**
+ * Allows configuration of the supported OAuth Flows.
+ * 
+ * This object MAY be extended with [Specification Extensions][1].
+ * 
+ * [1]: https://swagger.io/specification/#specificationExtensions
+ */
+export class OAuthFlowsObject {
+  /**
+   * Configuration for the OAuth Implicit flow.
+   */
+  implicit?: OAuthFlowObject;
+  /**
+   * Configuration for the OAuth Resource Owner Password flow.
+   */
+  password?: OAuthFlowObject;
+  /**
+   * Configuration for the OAuth Client Credentials flow.
+   * Previously called `application` in OpenAPI 2.0.
+   */
+  clientCredentials?: OAuthFlowObject;
+  /**
+   * Configuration for the OAuth Authorization Code flow.
+   * Previously called `accessCode` in OpenAPI 2.0.
+   */
+  authorizationCode?: OAuthFlowObject;
+}
